8afc144ee93e695404536a1e25c8c542c8a1c6ea
Nya2k <eudoravanya@gmail.com>
2025-04-25 23:40:56 +0700

    feat (login) : create login page

---
+++ fmoc-mobile-frontend/src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "@/components/modules/auth/login";
+
+export default function LoginPage() {
+    return (
+        <div className="flex items-center justify-center min-h-screen px-4">
+            <div className="w-full max-w-xs bg-white p-6 rounded-lg shadow-md">
+                <h2 className="heading-4 font-bold text-primary text-center mb-4">Log In</h2>
+                <LoginForm />
+            </div>
+        </div>
+    );
+}
