c78ec4c9921441695207009ccb10c3f1e38cd4ba
Yosef <yosef.nuraga@ui.ac.id>
2025-05-02 14:28:24 +0700

    Feat: fix bug2

--- fmoc-mobile-frontend/src/app/home/[id]/page.tsx
+++ fmoc-mobile-frontend/src/app/home/[id]/page.tsx
@@ -3,12 +3,10 @@
 import React from "react";
 import DetailRequest from "@/components/modules/request/detail-request";
 
-interface RequestPageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default function RequestPage({ params }: RequestPageProps) {
+export default function RequestPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   return <DetailRequest requestId={params.id} />;
 }
